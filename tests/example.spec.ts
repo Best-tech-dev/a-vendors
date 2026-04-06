4f5a378c132a3ced39b5ca8f0f192b6fb2f75c16
Ola-Oluwajuwon <okayode986@stu.ui.edu.ng>
2026-04-06 04:07:20 +0100

    Feature: update Playwright workflow to target dev branch and enhance example test case

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,14 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("should navigate to the home page and check the title", async ({
+  page,
+}) => {
+  // Go to the baseURL defined in config
+  await page.goto("/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Check that the heading contains specific text
+  const heading = page.locator("h1");
+  await expect(heading).toContainText(
+    "To get started, edit the page.tsx file.",
+  );
 });
